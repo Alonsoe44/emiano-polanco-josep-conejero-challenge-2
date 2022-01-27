33a897c00f4fa5d2a8922cd29fcd591d410a7e27
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-01-27 23:48:17 +0100

    Importig the clases for testing

--- web/tests/Card.test.js
+++ web/tests/Card.test.js
@@ -1,36 +1,6 @@
 import Component from "../scripts/components/Component.js";
 /* import series from "../series.js"; */
-
-export default class Card extends Component {
-  rutaPoster;
-  nombreSerie;
-  creadorSerie;
-  puntuacionSerie;
-  tituloAltSerie;
-
-  constructor(parentElement, className, serie) {
-    super(parentElement, className, "ul");
-
-    this.rutaPoster = serie.poster;
-    this.nombreSerie = serie.name;
-    this.creadorSerie = serie.creator + " (" + serie.year + ")";
-    this.tituloAltSerie = serie.name + " poster";
-    this.element.innerHTML = this.generateHTML();
-  }
-
-  generateHTML() {
-    return `<img
-                  class="serie__poster"
-                  src="${this.rutaPoster}"
-                  alt="${this.tituloAltSerie}"
-                />
-                <h4 class="serie__title">${this.nombreSerie}</h4>
-                <p class="serie__info">${this.creadorSerie}</p>
-                <ul class="score">
-                </ul>
-                <i class="fas fa-times-circle icon--delete"></i>`;
-  }
-}
+import Card from "../scripts/components/Card.js";
 
 describe("Given a Card class' constructor", () => {
   describe("When it's invoked with body, '' and serie", () => {
